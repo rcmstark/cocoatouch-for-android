5f4f830b5e025afd7146ff5c4c477f9d7d39df7f
Rafael Castro <castro.t13@gmail.com>
2016-09-15 01:34:54 -0300

    Added IBAction parser for ImageButton

--- cocoa-touch/src/main/java/com/hummingbird/objectivec/parser/IBActionParser.java
+++ cocoa-touch/src/main/java/com/hummingbird/objectivec/parser/IBActionParser.java
@@ -1,5 +1,7 @@
 package com.hummingbird.objectivec.parser;
-import com.hummingbird.cocoatouch.uikit.UIButton;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import com.hummingbird.cocoatouch.uikit.UIViewHierarchy;
 import com.hummingbird.objectivec.annotation.IBAction;
 import java.lang.reflect.Method;
@@ -15,14 +17,56 @@ public class IBActionParser
             parse(method, controller);
         }
     }
-
-    private static void parse(Method method, UIViewHierarchy controller)
+    private static void parse(final Method method, final UIViewHierarchy controller)
     {
         if(method.isAnnotationPresent(IBAction.class))
         {
             IBAction action = method.getAnnotation(IBAction.class);
-            UIButton button = (UIButton)controller.viewWithTag(action.value());
-            button.addTarget(controller, method, 0);
+            final Object button = controller.viewWithTag(action.value());
+            if (button == null) return;
+
+            if(Button.class.isAssignableFrom(button.getClass()))
+            {
+                parse((Button) button, method, controller);
+            }
+            else if(ImageButton.class.isAssignableFrom(button.getClass()))
+            {
+                parse((ImageButton)button, method, controller);
+            }
         }
+    }
+    private static void parse(final Button button, final Method method, final UIViewHierarchy controller)
+    {
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override public void onClick(View view)
+            {
+                try
+                {
+                    method.invoke(controller, button);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    private static void parse(final ImageButton button, final Method method, final UIViewHierarchy controller)
+    {
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override public void onClick(View view)
+            {
+                try
+                {
+                    method.invoke(controller, button);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
