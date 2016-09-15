fe0aa65ce920a9af5c9d929252464fdc182db652
Rafael Castro <castro.t13@gmail.com>
2016-09-14 23:06:35 -0300

    Added more delegate methods to UITextField

--- cocoa-touch/src/main/java/com/hummingbird/cocoatouch/uikit/UITextField.java
+++ cocoa-touch/src/main/java/com/hummingbird/cocoatouch/uikit/UITextField.java
@@ -1,22 +1,28 @@
 package com.hummingbird.cocoatouch.uikit;
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.AppCompatEditText;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
 
 
-public class UITextField extends EditText
+public class UITextField extends AppCompatEditText
 {
     private UITextFieldDelegate delegate = null;
     private TextWatcher observer = null;
+    private Drawable originalBackground;
 
-
-    public void init() {}
-
+    public void init()
+    {
+        this.originalBackground = getBackground();
+    }
     public void setText(String text)
     {
         super.setText(text);
@@ -30,9 +36,9 @@ public class UITextField extends EditText
     {
         this.delegate = delegate;
         if (this.delegate != null)
-            addListener();
+            addListeners();
         else
-            removeListener();
+            removeListeners();
     }
     public void setPlaceholder(String placeholder)
     {
@@ -48,6 +54,7 @@ public class UITextField extends EditText
     }
     public void becomeFirstResponder()
     {
+        this.textFieldDidStartEditing();
         this.setFocusableInTouchMode(true);
         this.requestFocus();
         Activity activity = UIApplication.sharedApplication().activity();
@@ -56,6 +63,7 @@ public class UITextField extends EditText
     }
     public void resignFirstResponder()
     {
+        this.textFieldDidFinishEditing();
         Activity activity = UIApplication.sharedApplication().activity();
         ((InputMethodManager)activity
                 .getSystemService(Context.INPUT_METHOD_SERVICE))
@@ -63,9 +71,35 @@ public class UITextField extends EditText
     }
 
     //
-    //Helpers
+    // Method called when user press back button
     //
-    private void addListener()
+    @Override public boolean onKeyPreIme(int keyCode, KeyEvent event)
+    {
+        if (keyCode == KeyEvent.KEYCODE_BACK)
+        {
+            textFieldDidFinishEditing();
+        }
+        return super.onKeyPreIme(keyCode, event);
+    }
+
+    //
+    // Listeners
+    //
+    private void addListeners()
+    {
+        addTextListener();
+        addKeyListener();
+        addTouchListener();
+        addFocusListener();
+    }
+    private void removeListeners()
+    {
+        removeTextChangedListener(observer);
+        this.setOnKeyListener(null);
+        this.setOnTouchListener(null);
+        this.setOnFocusChangeListener(null);
+    }
+    private void addTextListener()
     {
         final UITextFieldDelegate delegate = this.delegate;
         final UITextField textField = this;
@@ -89,9 +123,68 @@ public class UITextField extends EditText
         };
         addTextChangedListener(observer);
     }
-    private void removeListener()
+    private void addKeyListener()
     {
-        removeTextChangedListener(observer);
+        final UITextField textField = this;
+        this.setOnKeyListener(new OnKeyListener()
+        {
+            public boolean onKey(View view, int keyCode, KeyEvent event)
+            {
+                if (keyCode == KeyEvent.KEYCODE_ENTER)
+                {
+                    boolean shouldResign = true;
+                    if (delegate != null)
+                        shouldResign = delegate.textFieldShouldReturn(textField);
+                    if (shouldResign)
+                        resignFirstResponder();
+                    return shouldResign;
+                }
+                return false;
+            }
+        });
+    }
+    private void addTouchListener()
+    {
+        this.setOnTouchListener(new OnTouchListener()
+        {
+            @Override public boolean onTouch(View v, MotionEvent event)
+            {
+                if(MotionEvent.ACTION_UP == event.getAction())
+                {
+                    becomeFirstResponder();
+                }
+                return true;
+            }
+        });
+    }
+    private void addFocusListener()
+    {
+        this.setOnFocusChangeListener(new OnFocusChangeListener()
+        {
+            @Override public void onFocusChange(View v, boolean hasFocus)
+            {
+                if(!hasFocus)
+                    textFieldDidFinishEditing();
+            }
+        });
+    }
+
+    //
+    // Style + Notificate Delegates
+    //
+    private void textFieldDidFinishEditing()
+    {
+        setCursorVisible(false);
+        setBackgroundColor(Color.TRANSPARENT);
+        if(delegate != null)
+            delegate.textFieldDidEndEditing(this);
+    }
+    private void textFieldDidStartEditing()
+    {
+        setCursorVisible(true);
+        setBackground(originalBackground);
+        if (delegate != null)
+            delegate.textFieldDidBeginEditing(this);
     }
 
     //Java Trash Code
--- cocoa-touch/src/main/java/com/hummingbird/cocoatouch/uikit/UITextFieldDelegate.java
+++ cocoa-touch/src/main/java/com/hummingbird/cocoatouch/uikit/UITextFieldDelegate.java
@@ -3,7 +3,10 @@ import com.hummingbird.objectivec.annotation.Optional;
 
 public interface UITextFieldDelegate
 {
-    @Optional void textFieldDidChange(UITextField textField);
-    @Optional void textFieldShouldChange(UITextField textField, CharSequence s, int start, int before, int count);
-    @Optional void textFieldBeforeChange(UITextField textField, CharSequence s, int start, int count, int after);
+    @Optional boolean textFieldShouldReturn(UITextField textField);
+    @Optional void    textFieldDidEndEditing(UITextField textField);
+    @Optional void    textFieldDidBeginEditing(UITextField textField);
+    @Optional void    textFieldDidChange(UITextField textField);
+    @Optional void    textFieldShouldChange(UITextField textField, CharSequence s, int start, int before, int count);
+    @Optional void    textFieldBeforeChange(UITextField textField, CharSequence s, int start, int count, int after);
 }
